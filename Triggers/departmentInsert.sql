bcfb28351b1908c04308bafaf3f02ec50dd4b150
YerkanatSultanov <erkanatsultanov1999@gmail.com>
2022-12-17 04:49:15 +0600

    очень много изменений

--- Triggers/departmentInsert.sql
+++ Triggers/departmentInsert.sql
@@ -1,8 +1,12 @@
 create trigger departmentInsert
-    after insert on doctor_details
-    when  (select department_id from department
-                            where department_name = new.department_name) is not null
-    begin
-    insert into doctor_department(department_id, doctor_Id) values ((select department_id from department
-                            where department_name = new.department_name), new.doctorId);
-        end;
+    after insert
+    on doctor_details
+    when (select department_id
+          from department
+          where department_name = new.department_name) is not null
+begin
+    insert into doctor_department(department_id, doctor_Id)
+    values ((select department_id
+             from department
+             where department_name = new.department_name), new.doctorId);
+end;
